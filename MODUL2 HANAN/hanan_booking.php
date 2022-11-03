4f462716bb6b6229313e49cf8c7780735471a0db
arifraihana01 <arif.raihannabil90@gmail.com>
2022-11-03 14:03:56 +0700

    u

--- MODUL2 HANAN/hanan_booking.php
+++ MODUL2 HANAN/hanan_booking.php
@@ -1,114 +1,114 @@
-<!doctype html>
-<html lang="en">
+    <!doctype html>
+    <html lang="en">
 
-<head>
-    <?php
-    require "database/hanan_database.php";
-?>
-  <head>
+    <head>
+        <?php
+        require "database/hanan_database.php";
+    ?>
+    <head>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>EAD RENT | HANAN_1202204042</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>EAD RENT | HANAN_1202204042</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
-</head>
+    </head>
 
-<body>
-    <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-        <div class="container-fluid">
-            <div class="collapse navbar-collapse justify-content-md-center">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="hanan_home.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="hanan_booking.php">Booking</a>
-                    </li>
-                </ul>
+    <body>
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse justify-content-md-center">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="hanan_home.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="hanan_booking.php">Booking</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- End Navbar -->
+
+        <!-- Title -->
+        <div class="mt-2 mb-2 container">
+            <h1 class="p-1" style="text-align: center;">Rent your car now!</h1>
+        </div>
+        <!-- End Title -->
+
+        <div class="container card shadow-sm">
+            <div class="d-flex justify-content-around">
+                <div class="col-6 ps-5 pe-5 d-flex align-items-center">
+                    <?php
+                    if (isset($_GET["images"])) {
+                    ?><img class="" width="100%" src="<?= $_GET["images"]; ?>" alt="">
+                    <?php
+                    } else {
+                    ?><img width="100%" src="images/img-fortuner.png" alt="">
+                    <?php }?>
+                </div>
+                <div class="col-6 ps-5 pe-5">
+                    <ul type="none">
+                        <form action="hanan_my_booking.php" method="post">
+                            <li class="m-1">
+                                <label for="name">Name</label><br>
+                                <input class="form-control bg-light" type="text" name="name" id="name" placeholder="<?= $name;?>" readonly>
+                            </li>
+                            <li class="m-1">
+                                <label for="booking">Book Date</label><br>
+                                <input class="form-control" type="date" name="booking" id="booking">
+                            </li>
+                            <li class="m-1">
+                                <label for="time">Start Time</label><br>
+                                <input class="form-control" type="time" name="time" id="time">
+                            </li>
+                            <li class="m-1">
+                                <label for="day">Duration (Days)</label><br>
+                                <input class="form-control" type="number" name="day" id="day" min="1">
+                            </li>
+                            <li class="m-1">
+                                <label for="car_type">Car Type</label>
+                                <selects class="form-select" name="car_type" id="car_type">
+                                    <option selected>Choose...</option>
+                                    <?php foreach ($carlist as $car_list) : ?>
+                                        <?php
+                                        if ($car_list["car"] == $_GET["car"]) {
+                                        ?><option selected="selected" value="<?= $car_list["car"]; ?>"><?= $car_list["car"]; ?></option>
+                                        <?php
+                                        } else {
+                                        ?><option value="<?= $car_list["car"]; ?>"><?= $car_list["car"]; ?></option>
+                                        <?php } ?>
+                                    <?php endforeach; ?>
+                                </selects>
+                            </li>
+                            <li class="m-1">
+                                <label for="phone">Phone Number</label><br>
+                                <input class="form-control" type="number" name="phone" id="phone" min="0">
+                            </li>
+                            <li class="m-1">
+                                <label for="service">Add Service(s)</label><br>
+                                <input class="form-check-input m-1" type="checkbox" name="service[]" value="Health protocol" id="health"><label class="form-check-label" for="health">Health protocol / Rp25.000</label><br>
+                                <input class="form-check-input m-1" type="checkbox" name="service[]" value="Driver" id="driver"><label class="form-check-label" for="driver">Driver / Rp100.000</label><br>
+                                <input class="form-check-input m-1" type="checkbox" name="service[]" value="Fuel Filled" id="fuel"><label class="form-check-label" for="fuel">Fuel Filled / Rp.250.000</label><br>
+                            </li>
+                            <li class="text-center">
+                                <button class="btn btn-success justify-content-around mt-3" style="width: 100%;" type="submit">Book</button>
+                            </li>
+                        </form>
+                    </ul>
+                </div>
             </div>
         </div>
-    </nav>
-    <!-- End Navbar -->
 
-    <!-- Title -->
-    <div class="mt-2 mb-2 container">
-        <h1 class="p-1" style="text-align: center;">Rent your car now!</h1>
-    </div>
-    <!-- End Title -->
+        <footer class="fixed-bottom text-center pt-3 bg-light">
+            <p style="text-align: center; color: lightslategray;">
+                Created by <?php echo $name; ?>
+            </p>
+        </footer>
 
-    <div class="container card shadow-sm">
-        <div class="d-flex justify-content-around">
-            <div class="col-6 ps-5 pe-5 d-flex align-items-center">
-                <?php
-                if (isset($_GET["images"])) {
-                ?><img class="" width="100%" src="<?= $_GET["images"]; ?>" alt="">
-                <?php
-                } else {
-                ?><img width="100%" src="images/img-fortuner.png" alt="">
-                <?php }?>
-            </div>
-            <div class="col-6 ps-5 pe-5">
-                <ul type="none">
-                    <form action="hanan_my_booking.php" method="post">
-                        <li class="m-1">
-                            <label for="name">Name</label><br>
-                            <input class="form-control bg-light" type="text" name="name" id="name" placeholder="<?= $name;?>" readonly>
-                        </li>
-                        <li class="m-1">
-                            <label for="booking">Book Date</label><br>
-                            <input class="form-control" type="date" name="booking" id="booking">
-                        </li>
-                        <li class="m-1">
-                            <label for="time">Start Time</label><br>
-                            <input class="form-control" type="time" name="time" id="time">
-                        </li>
-                        <li class="m-1">
-                            <label for="day">Duration (Days)</label><br>
-                            <input class="form-control" type="number" name="day" id="day" min="1">
-                        </li>
-                        <li class="m-1">
-                            <label for="car_type">Car Type</label>
-                            <select class="form-select" name="car_type" id="car_type">
-                                <option selected>Choose...</option>
-                                <?php foreach ($carlist as $car_list) : ?>
-                                    <?php
-                                    if ($car_list["car"] == $_GET["car"]) {
-                                    ?><option selected="selected" value="<?= $car_list["car"]; ?>"><?= $car_list["car"]; ?></option>
-                                    <?php
-                                    } else {
-                                    ?><option value="<?= $car_list["car"]; ?>"><?= $car_list["car"]; ?></option>
-                                    <?php } ?>
-                                <?php endforeach; ?>
-                            </select>
-                        </li>
-                        <li class="m-1">
-                            <label for="phone">Phone Number</label><br>
-                            <input class="form-control" type="number" name="phone" id="phone" min="0">
-                        </li>
-                        <li class="m-1">
-                            <label for="service">Add Service(s)</label><br>
-                            <input class="form-check-input m-1" type="checkbox" name="service[]" value="Health protocol" id="health"><label class="form-check-label" for="health">Health protocol / Rp25.000</label><br>
-                            <input class="form-check-input m-1" type="checkbox" name="service[]" value="Driver" id="driver"><label class="form-check-label" for="driver">Driver / Rp100.000</label><br>
-                            <input class="form-check-input m-1" type="checkbox" name="service[]" value="Fuel Filled" id="fuel"><label class="form-check-label" for="fuel">Fuel Filled / Rp.250.000</label><br>
-                        </li>
-                        <li class="text-center">
-                            <button class="btn btn-success justify-content-around mt-3" style="width: 100%;" type="submit">Book</button>
-                        </li>
-                    </form>
-                </ul>
-            </div>
-        </div>
-    </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </body>
 
-    <footer class="fixed-bottom text-center pt-3 bg-light">
-        <p style="text-align: center; color: lightslategray;">
-            Created by <?php echo $name; ?>
-        </p>
-    </footer>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
-
-</html>
+    </html>
--- MODUL2 HANAN/hanan_home.php
+++ MODUL2 HANAN/hanan_home.php
@@ -1,115 +1,115 @@
-<!doctype html>
-<html lang="en">
+    <!doctype html>
+    <html lang="en">
 
-<?php
-    require "database/hanan_database.php";
-?>
-  <head>
+    <?php
+        require "database/hanan_database.php";
+    ?>
+    <head>
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>EAD RENT | HANAN_1202204042</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>EAD RENT | HANAN_1202204042</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 
-  </head>
+    </head>
 
-  <body>
+    <body>
 
-      <!-- Navbar -->
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-        <div class="container-fluid">
-            <div class="collapse navbar-collapse justify-content-md-center">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="hanan_home.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="hanan_booking.php">Booking</a>
-                    </li>
-                </ul>
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse justify-content-md-center">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="hanan_home.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="hanan_booking.php">Booking</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- End Navbar -->
+        
+        <!-- Title -->
+        <div>
+            <p class="m-2" style="text-align: center; font-size: 25px">WELCOME TO EAD RENT</p>
+            <p class="p-1" style="text-align: center;">Find your best deal, here!</p>
+        </div>
+        <!-- End Title -->
+
+        <!-- Content -->
+        <div class="container">
+            <div class="d-flex justify-content-around">
+                <div class="col-3 m-1">
+                    <div class="card shadow-sm rounded">
+                        <img src="<?php echo $carlist[0]["images"]; ?>" alt="">
+                        <div class="m-3">
+                            <p class="m-0" style="font-size: 25px"><?php echo $carlist[0]["car"]; ?></p>
+                            <p class="m-0" style="color: grey">Rp. <?php echo $carlist[0]["price"]; ?> / Day</p>
+                        </div>
+                        <hr class="m-0">
+                        <p class="m-2" style="text-align: center; color: blue; font-weight: bold;">7 Kursi</p>
+                        <hr class="m-0">
+                        <p class="m-2" style="text-align: center; color: blue; font-weight: bold;">2755 CC</p>
+                        <hr class="m-0">
+                        <p class="m-2" style="text-align: center; color: blue; font-weight: bold;">Manual</p>
+                        <hr class="m-0">
+                        <div class="text-center bg-light">
+                            <button class="m-2 btn btn-primary" type="submit"><a href="hanan_booking.php?images=<?= $carlist[0]["images"]; ?>&car=<?= $carlist[0]["car"]; ?>" style="color: white; text-decoration: none;">Book Now</a></button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3 m-1 ms-2 me-2">
+                    <div class="card shadow-sm rounded">
+                        <img src="<?php echo $carlist[1]["images"]; ?>" alt="">
+                        <div class="m-3">
+                            <p class="m-0" style="font-size: 25px"><?php echo $carlist[1]["car"]; ?></p>
+                            <p class="m-0" style="color: grey">Rp. <?php echo $carlist[1]["price"]; ?> / Day</p>
+                        </div>
+                        <hr class="m-0">
+                        <p class="m-2" style="text-align: center; color: blue; font-weight: bold;">7 Kursi</p>
+                        <hr class="m-0">
+                        <p class="m-2" style="text-align: center; color: blue; font-weight: bold;">1500 CC</p>
+                        <hr class="m-0">
+                        <p class="m-2" style="text-align: center; color: blue; font-weight: bold;">Manual</p>
+                        <hr class="m-0">
+                        <div class="text-center bg-light">
+                            <button class="m-2 btn btn-primary" type="submit"><a href="hanan_booking.php?images=<?= $carlist[1]["images"]; ?>&car=<?= $carlist[1]["car"]; ?>" style="color: white; text-decoration: none;">Book Now</a></button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-3 m-1">
+                    <div class="card shadow-sm rounded">
+                        <img src="<?php echo $carlist[2]["images"]; ?>" alt="">
+                        <div class="m-3">
+                            <p class="m-0" style="font-size: 25px"><?php echo $carlist[2]["car"]; ?></p>
+                            <p class="m-0" style="color: grey">Rp. <?php echo $carlist[2]["price"]; ?> / Day</p>
+                        </div>
+                        <hr class="m-0">
+                        <p class="m-2" style="text-align: center; color: blue; font-weight: bold;">7 Kursi</p>
+                        <hr class="m-0">
+                        <p class="m-2" style="text-align: center; color: blue; font-weight: bold;">1496 CC</p>
+                        <hr class="m-0">
+                        <p class="m-2" style="text-align: center; color: blue; font-weight: bold;">Automatic</p>
+                        <hr class="m-0">
+                        <div class="text-center bg-light">
+                            <button class="m-2 btn btn-primary" type="submit"><a href="hanan_booking.php?images=<?= $carlist[2]["images"]; ?>&car=<?= $carlist[2]["car"]; ?>" style="color: white; text-decoration: none;">Book Now</a></button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </nav>
-      <!-- End Navbar -->
-    
-      <!-- Title -->
-      <div>
-        <p class="m-2" style="text-align: center; font-size: 25px">WELCOME TO EAD RENT</p>
-        <p class="p-1" style="text-align: center;">Find your best deal, here!</p>
-    </div>
-      <!-- End Title -->
+        <!-- End Content -->
 
-      <!-- Content -->
-      <div class="container">
-        <div class="d-flex justify-content-around">
-            <div class="col-3 m-1">
-                <div class="card shadow-sm rounded">
-                    <img src="<?php echo $carlist[0]["images"]; ?>" alt="">
-                    <div class="m-3">
-                        <p class="m-0" style="font-size: 25px"><?php echo $carlist[0]["car"]; ?></p>
-                        <p class="m-0" style="color: grey">Rp. <?php echo $carlist[0]["price"]; ?> / Day</p>
-                    </div>
-                    <hr class="m-0">
-                    <p class="m-2" style="text-align: center; color: blue; font-weight: bold;">7 Kursi</p>
-                    <hr class="m-0">
-                    <p class="m-2" style="text-align: center; color: blue; font-weight: bold;">2755 CC</p>
-                    <hr class="m-0">
-                    <p class="m-2" style="text-align: center; color: blue; font-weight: bold;">Manual</p>
-                    <hr class="m-0">
-                    <div class="text-center bg-light">
-                        <button class="m-2 btn btn-primary" type="submit"><a href="hanan_booking.php?images=<?= $carlist[0]["images"]; ?>&car=<?= $carlist[0]["car"]; ?>" style="color: white; text-decoration: none;">Book Now</a></button>
-                    </div>
-                </div>
-            </div>
-            <div class="col-3 m-1 ms-2 me-2">
-                <div class="card shadow-sm rounded">
-                    <img src="<?php echo $carlist[1]["images"]; ?>" alt="">
-                    <div class="m-3">
-                        <p class="m-0" style="font-size: 25px"><?php echo $carlist[1]["car"]; ?></p>
-                        <p class="m-0" style="color: grey">Rp. <?php echo $carlist[1]["price"]; ?> / Day</p>
-                    </div>
-                    <hr class="m-0">
-                    <p class="m-2" style="text-align: center; color: blue; font-weight: bold;">7 Kursi</p>
-                    <hr class="m-0">
-                    <p class="m-2" style="text-align: center; color: blue; font-weight: bold;">1500 CC</p>
-                    <hr class="m-0">
-                    <p class="m-2" style="text-align: center; color: blue; font-weight: bold;">Manual</p>
-                    <hr class="m-0">
-                    <div class="text-center bg-light">
-                        <button class="m-2 btn btn-primary" type="submit"><a href="hanan_booking.php?images=<?= $carlist[1]["images"]; ?>&car=<?= $carlist[1]["car"]; ?>" style="color: white; text-decoration: none;">Book Now</a></button>
-                    </div>
-                </div>
-            </div>
-            <div class="col-3 m-1">
-                <div class="card shadow-sm rounded">
-                    <img src="<?php echo $carlist[2]["images"]; ?>" alt="">
-                    <div class="m-3">
-                        <p class="m-0" style="font-size: 25px"><?php echo $carlist[2]["car"]; ?></p>
-                        <p class="m-0" style="color: grey">Rp. <?php echo $carlist[2]["price"]; ?> / Day</p>
-                    </div>
-                    <hr class="m-0">
-                    <p class="m-2" style="text-align: center; color: blue; font-weight: bold;">7 Kursi</p>
-                    <hr class="m-0">
-                    <p class="m-2" style="text-align: center; color: blue; font-weight: bold;">1496 CC</p>
-                    <hr class="m-0">
-                    <p class="m-2" style="text-align: center; color: blue; font-weight: bold;">Automatic</p>
-                    <hr class="m-0">
-                    <div class="text-center bg-light">
-                        <button class="m-2 btn btn-primary" type="submit"><a href="hanan_booking.php?images=<?= $carlist[2]["images"]; ?>&car=<?= $carlist[2]["car"]; ?>" style="color: white; text-decoration: none;">Book Now</a></button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-      <!-- End Content -->
-
-      <!-- Footer -->
-      <footer class="fixed-bottom text-center pt-3 bg-light">
-        <p style="text-align: center; color: lightslategray;">
-            Created by HANAN_1202204042
-        </p>
-    </footer>
-      <!-- End Footer -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-  </body>
-</html>
+        <!-- Footer -->
+        <footer class="fixed-bottom text-center pt-3 bg-light">
+            <p style="text-align: center; color: lightslategray;">
+                Created by HANAN_1202204042
+            </p>
+        </footer>
+        <!-- End Footer -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    </body>
+    </html>
